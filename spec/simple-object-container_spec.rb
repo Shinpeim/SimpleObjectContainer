59028602197247c20066d0cc2e38b429897b14b3
Shinpei Maruyama <shinpeim@gmail.com>
2013-02-13 21:46:48 +0900

    Add register/get method
    First, SimpleObjectContainer#register should be called with a class.
    Then, SimpleObjectContainer#get return the instance of the class if called with the class
    
        container = SimpleObjectContainer.new
        container.register(SomeClass)
        container.get(SomeClass) # => instace of SomeClass

--- spec/simple-object-container_spec.rb
+++ spec/simple-object-container_spec.rb
@@ -1,7 +1,26 @@
+require "spec_helper"
 require "simple-object-container"
 
 describe SimpleObjectContainer do
-  it "can be created" do
-    SimpleObjectContainer.new.should be_instance_of SimpleObjectContainer
+  describe "Object interface" do
+    let (:container) { SimpleObjectContainer.new }
+    it "can be created" do
+      container.should be_instance_of SimpleObjectContainer
+    end
+
+    context "when SomeClass is registered" do
+      before do
+        container.register(SomeClass)
+      end
+      describe "#get" do
+        context "given SomeClass"
+        it "return a instance of SomeClass" do
+          container.get(SomeClass).should be_instance_of SomeClass
+        end
+        it "always return same object" do
+          container.get(SomeClass).should be_equal container.get(SomeClass)
+        end
+      end
+    end
   end
 end
